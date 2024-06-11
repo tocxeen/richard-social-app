1d5ca0bad06726c184ee56b6fbda1c2dd92e532e
tocxeen <tocxeen@outlook.com>
2024-06-11 10:19:14 +0200

    packages setup

---
+++ apps/richard-social-app/src/app/shared/frame/frame.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-frame',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './frame.component.html',
+  styleUrl: './frame.component.css',
+})
+export class FrameComponent {}
